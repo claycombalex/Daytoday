66f76aa56a832f2b2dbae63830fc1cf1b1160a55
claycombalex <claycombalex@gmail.com>
2021-07-25 20:47:47 -0400

    7/25/2021

--- daytoday/src/daytoday/Event.java
+++ daytoday/src/daytoday/Event.java
@@ -8,24 +8,43 @@ public class Event {
 	private int importance;						//how important is this event (1-5)
 	private String tag;							//organizational tag of this event
 	private ArrayList<String> keywords;			//keywords to find this event when searching for it
+	private int startTime;						//when does event start
+	private int endTime;						//when does event end
+	private boolean timeSensitive;				//does this event have to occur at a specific time
+	private int hoursToComplete;
 	
 	private ArrayList<Event> subevents;			//child events
 	private Event parent;
 	
 	private boolean calendar, todo, schedule;	//where will this event appear
 	
-	public Event(String name, boolean routine, int importance, boolean[] appear) {
+	private int[][] customInfo;		//2D array to create custom ranges
+	
+	public Event(String name, boolean routine, int importance, boolean[] appear, String tag, ArrayList<String> keywords, 
+			int startTime, int endTime, boolean timeSensitive, int hoursToComplete, Event parent, int[][] customInfo) {
 		
 		this.name = name;
 		this.routine = routine;
 		this.importance = importance;
+		this.tag = tag;
+		this.keywords = keywords;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.timeSensitive = timeSensitive;
+		this.hoursToComplete = hoursToComplete;
+		
+		if(routine) {
+			this.customInfo = customInfo;
+		} else {
+			this.customInfo = null;
+		}
 		
 		calendar = appear[0];
 		todo = appear[1];
 		schedule = appear[2];
 		
 		subevents = new ArrayList<Event>();
-		parent = null;
+		this.parent = parent;
 	}
 	
 	public ArrayList<Event> getSubevents() {
--- daytoday/src/daytoday/SceneController.java
+++ daytoday/src/daytoday/SceneController.java
@@ -13,29 +13,51 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class SceneController implements Initializable {
 	
+	//used for scene management
 	private String currentScene;
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
+	//css for background color and various style
 	String css = this.getClass().getResource("application.css").toExternalForm();
 	
+	//control nodes used for creating new user
 	public ComboBox<String> themeBox;
 	public ComboBox<String> importantBox;
 	public ComboBox<String> bedtimeBox;
+	
+	//Control nodes used for building events
 	public ComboBox<String> reoccurBox;
 	public ComboBox<String> setTimeBox;
-	public AnchorPane reoccuringElems;
-	public AnchorPane oneTimeElems;
-	public AnchorPane startTimeElem;
-	public AnchorPane endTimeElem;
+	public TextField eventField;
+	public DatePicker startDateField;
+	public TextField startHourField;
+	public TextField startMinuteField;
+	public DatePicker endDateField;
+	public TextField endHourField;
+	public TextField endMinuteField;
+	public TextField totalHoursField;
+	public CheckBox showOnCalendar;
+	public CheckBox showOnCountdown;
+	public CheckBox showOnSchedule;
+	public CheckBox showOnToDo;
+	public ChoiceBox<String> importantBox2;
+	public TextField tagField;
+	public TextArea keywordsField;
 	
+	//lists for choice boxes
 	ObservableList<String> themeSelect = FXCollections.observableArrayList("Light Theme (Default)", "Dark Theme");
 	ObservableList<String> importantList = FXCollections.observableArrayList("Required (6)", "Very important (5)", "Important (4)", "Somewhat important (3)", "Not very important (2)", "Not important at all (1)");
 	ObservableList<String> bedtimeList = FXCollections.observableArrayList("AM", "PM");
@@ -119,5 +141,25 @@ public class SceneController implements Initializable {
 		if(choice.contentEquals("No (only occurs once)")) {
 			System.out.println(reoccurBox.getId());
 		}
+	}
+	
+	public void createEvent() {
+		
+		boolean routineVal = true;
+		if(reoccurBox.getValue().contentEquals("No (only occurs once)"))
+			routineVal = false;
+		
+		int importantVal = 0;
+		String tempStr = "";
+		Scanner importantScan = new Scanner(importantBox2.getValue());
+		importantScan.useDelimiter("(");
+		tempStr = importantScan.next();
+		importantVal = Integer.parseInt(tempStr.substring(0, 1));
+		
+		boolean[] appearVal = {showOnCalendar.isSelected(), showOnCountdown.isSelected(), 
+				showOnSchedule.isSelected(), showOnToDo.isSelected()};
+		
+		
+		Event theEvent = new Event(eventField.getText(), routineVal, importantVal, appearVal, tagField.getText(), );
 	}
 }
